a1f25a59dbbb80c85387e78873efb72622811bb4
shmuel-blitz <shmueld@kugelblitz.one>
2023-05-03 21:11:51 +0300

    simpler

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 // add text files here
-const exportList = [
-    'tldr',
-]
-
-const exportMap = {}
-
-exportList.forEach(fileName=> exportMap[fileName] = require(`./${fileName}.text.json`))
-
-module.exports = exportMap
+module.exports = {
+    tldr: require('./tldr.text.json')
+}
