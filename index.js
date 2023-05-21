0b9f9968d8905faa52595288183bf3ceb52d3f01
aviel-lanzman <avielnzmn@gmail.com>
2023-05-21 12:18:34 +0300

    export helpers methods

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // add text files here
 module.exports = {
   tldr: require('./tldr.text.json'),
-  gpt: require('./gpt.text.json'),
   share: require('./share.text.json'),
+  gpt: require('./gpt.text.js'),
+  helper: require('./helper.js'),
 }
