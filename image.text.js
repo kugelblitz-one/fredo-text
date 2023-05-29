fe249edd1918c4076e518b4c1f97f70fa1498bc0
shmuel-blitz <shmueld@kugelblitz.one>
2023-05-29 16:10:12 +0300

    fix

--- image.text.js
+++ image.text.js
@@ -1,9 +1,7 @@
 const { newLine, bold } = require('./helper.js')
 
 module.exports = {
-  processor: `I could not understand that or something is wrong..${newLine()}Please try to rephrase your prompt and send it again.${newLine(
-    2
-  )}If the issue continues please submit a bug.`,
+  processor: `Generating your image...`,
   error: 'I could not understand that..  try to rephrase and send it again.',
   help: `🏞️🏞️🏞️${newLine()}Need a quick image reference? Fredo creates images from text captions for a wide range of concepts expressible in natural language.${newLine()}💡 Tip: start with the word image and specify after that description of your desired image${newLine()}${bold(
     'Examples:'
