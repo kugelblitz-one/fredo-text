f90b94f8999ee7101aa3deb81948e7e40470ae1c
aviel-lanzman <avielnzmn@gmail.com>
2023-06-04 16:19:35 +0300

    add delete text

---
+++ delete.text.js
@@ -0,0 +1,8 @@
+module.exports = {
+  areYouSureDelete: `Are you sure you want to delete your account and all its data? This can not be undone.`,
+  noDelete: `Yay. Happy to see you stay. \nEnjoy Fredo.`,
+  yesDelete: `We have deleted your account data from all our logs.
+  \n
+  Would you spare a minute to let us know why you are leaving?`,
+  afterDelete: `Thanks for using Fredo! At any point if you want to start using Fredo again just come back and send us 'Hi'.`,
+}
