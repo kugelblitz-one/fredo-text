aac231f66f6d23ac42860f0d5298b3b2d6d73886
lotem vanunu <lotem@closeapp.co.il>
2023-06-04 14:59:42 +0300

    fix imgae.help

--- helper.js
+++ helper.js
@@ -1,6 +1,8 @@
 const bold = text => `*${text}*`
+const italicize  = text => `_${text}_`
 const newLine = (lins = 1) => `\n`.repeat(lins)
 module.exports = {
   bold,
   newLine,
+  italicize
 }
--- image.text.js
+++ image.text.js
@@ -1,11 +1,9 @@
-const { newLine, bold } = require('./helper.js')
+const { newLine, bold, italicize } = require('./helper.js')
 
 module.exports = {
   processor: `Generating your image...`,
   error: 'I could not understand that..  try to rephrase and send it again.',
-  help: `🏞️🏞️🏞️${newLine()}Need a quick image reference? Fredo creates images from text captions for a wide range of concepts expressible in natural language.${newLine()}💡 Tip: start with the word image and specify after that description of your desired image${newLine()}${bold(
-    'Examples:'
-  )}${newLine()}image an illustration of a baby daikon radish in a tutu walking a dog${newLine()}_Send ${bold(
-    'HELP'
-  )} to return to main menu_`,
+  help: `🏞️🏞️🏞️${newLine()}Need a quick image reference? Fredo creates images from text captions for a wide range of concepts expressible in natural language.${newLine(2)}💡 Tip: start with the word image and specify after that description of your desired image${newLine(2)}`+
+      `${bold('Examples:')}${newLine()}image an illustration of a baby daikon radish in a tutu walking a dog${newLine(2)}` +
+      italicize(`Send ${bold('HELP')} to return to main menu`),
 }
