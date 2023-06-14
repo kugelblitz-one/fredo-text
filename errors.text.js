4086e9be086436b6c4b3260ffd42ffe6e588a692
Lotem Vanunu <98812015+lotem-vanunu@users.noreply.github.com>
2023-06-14 14:41:55 +0300

    Update errors.text.js

--- errors.text.js
+++ errors.text.js
@@ -1,3 +1,4 @@
 module.exports = {
   generic: `Ooops....🤒\nIt's not you, it's us.\nSeems like we ran into a problem. \nWe are on it! \nNote: in case you feel stuck try the *Help* command`,
+  invalid_command : "You used an unrecognized '/' Fredo command."
 }
