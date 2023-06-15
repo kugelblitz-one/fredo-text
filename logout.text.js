7a83cfb0252f84b967f0178f6fea29ba9ead7923
lotem vanunu <lotem@closeapp.co.il>
2023-06-15 09:36:27 +0300

    add new line

--- logout.text.js
+++ logout.text.js
@@ -2,6 +2,6 @@ module.exports = {
   areYouSureLogout(platform = 'Fredo') {
     return `Are you sure you want to logout from ${platform}? \n(This means your current saved settings will be lost)`
   },
-  yesLogout: `🔌 Ok You are logged out now.\n'Before you go, you can explore some other skills Fredo has to offer.`,
+  yesLogout: `🔌 Ok You are logged out now.\n\n'Before you go, you can explore some other skills Fredo has to offer.`,
   noLogout: 'Yay. Happy to see you stay. Enjoy Fredo.',
 }
