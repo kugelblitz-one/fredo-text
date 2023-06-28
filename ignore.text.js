7a9cb9faa4b782f1e2e689261f0a994755885bdc
lotem vanunu <lotem@closeapp.co.il>
2023-06-28 15:30:55 +0300

    ignore.text.js

--- ignore.text.js
+++ ignore.text.js
@@ -9,17 +9,17 @@ module.exports = {
   "ignore_works_only_for_notes": "Ignore works only for notes.\n" +
     "You should setup notes first and then control the forwarding settings.\n\n" +
     "👉 Send  *NOTES*  if you want to start using notes forwarding.",
-  change_ignore_to_default_behavior: () => {
+  change_ignore_to_default_behavior: (platform_name) => {
     return `Currently, all your messages are forwarded by default to ${platform_name}.` +
       "You can change this default behavior so only messages with ! are forwarded." +
       "Would you like to keep this default, or change it?"
   },
-  keep_note_as_default: () => {
+  keep_note_as_default: (platform_name) => {
     return "No problem.\n" +
       `All your notes will be sent to ${platform_name} as before.` +
       "You can always add ! at the start note, to exclude it from notes"
   },
-  change_to_ignore_mode: () => {
+  change_to_ignore_mode: (platform_name) => {
     return "Done.\n" +
       "Fredo will no longer forward your notes by default.\n" +
       `Add ! before a note to forward it just this time to ${platform_name} .`
