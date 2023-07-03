0ce92b478095d7f8672cba25056b3ffd32e8d33c
avielCloseapp <aviel@closeapp.co.il>
2023-07-03 16:12:21 +0300

    calendar

---
+++ calendar.text.js
@@ -0,0 +1,8 @@
+module.exports = {
+  followLink: link => `Follow this link to add this reminder to your calendar.  ${link}.`,
+  WouldYouLike: 'Would you like reminders to show up in your calendar?',
+  whatCalendarUse: 'What calendar do you use?',
+  includeNextTime: 'Would you like me to include such links next time you set a reminder?',
+  WillDo: 'Will do.',
+  reminderRequestOption: `No problem. If you ever change your mind, just send "reminder" and follow the instructions.`,
+}
