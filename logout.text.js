6d73536d7f62343e0db8f0226bd0820424e9b91c
assafza <assaf@papayaapps.io>
2023-07-20 00:14:59 +0300

    Update logout.text.js

--- logout.text.js
+++ logout.text.js
@@ -2,6 +2,6 @@ module.exports = {
   areYouSureLogout(platform = 'Fredo') {
     return `Are you sure you want to logout from ${platform}? \n(This means your current saved settings will be lost)`
   },
-  yesLogout: `🔌 Ok You are logged out now.\n\nBefore you go, you can explore some other skills Fredo has to offer.`,
-  noLogout: 'Yay. Happy to see you stay. Enjoy Fredo.',
+  yesLogout: `🔌 Ok Your notes forwarding setup has been deleted now.\n\nIn case you want to setup notes forwarding again send *notes*.`,
+  noLogout: 'Sure. Enjoy Fredo.',
 }
