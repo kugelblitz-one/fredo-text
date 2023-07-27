c114c4fb249d2d9d6e346f2292d1349b9e066012
shmuel-blitz <shmueld@kugelblitz.one>
2023-07-27 10:42:07 +0300

    unavailable

--- errors.text.js
+++ errors.text.js
@@ -1,4 +1,5 @@
 module.exports = {
   generic: `Ooops....🤒\nIt's not you, it's us.\nSeems like we ran into a problem. \nWe are on it! \nNote: in case you feel stuck try the *Help* command`,
-  invalid_command : "You used an unrecognized '/' Fredo command."
+  invalid_command : "You used an unrecognized '/' Fredo command.",
+  unavailable: "Sorry this feature is temporary unavailable, Please try again later"
 }
