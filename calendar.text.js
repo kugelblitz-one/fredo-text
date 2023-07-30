12b607861092de91fab35281e9d2173909bc24e9
assafza <assaf@papayaapps.io>
2023-07-30 11:04:48 +0300

    Update calendar.text.js

--- calendar.text.js
+++ calendar.text.js
@@ -1,5 +1,5 @@
 module.exports = {
-  followLink: link => `Follow this link to add this reminder to your calendar.  ${link}.`,
+  followLink: link => `Add to calendar: ${link}.`,
   WouldYouLike: 'Would you like reminders to show up in your calendar?',
   whatCalendarUse: 'What calendar do you use?',
   includeNextTime: 'Would you like me to include such links next time you set a reminder?',
