a25b8a7f0324db86f410822696f3d03485481a74
Harel Sarag <harel@kugelblitz.one>
2023-08-09 11:25:18 +0300

    new text to logout notes

--- logout.text.js
+++ logout.text.js
@@ -2,6 +2,6 @@ module.exports = {
   areYouSureLogout(platform = 'Fredo') {
     return `Are you sure you want to reset your ${platform} setup? \n(This means your current saved settings will be lost)`
   },
-  yesLogout: `🔌 Ok Your notes forwarding setup has been deleted now.\n\nIn case you want to setup notes forwarding again send *notes*.`,
+  yesLogout: `🔌 Ok Your notes forwarding setup has been deleted now.\n\nAnything you send Fredo as a note will be kept here like a group with yourself.\n\nIn case you want to setup notes forwarding again send *notes*.`,
   noLogout: 'Sure. Enjoy Fredo.',
 }
