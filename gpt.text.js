5db3f843a9c9cc0097b9bd69e0741a64522628b7
assafza <assaf@papayaapps.io>
2023-08-20 01:35:33 +0300

    Update gpt.text.js

--- gpt.text.js
+++ gpt.text.js
@@ -4,30 +4,24 @@ module.exports = {
   processor: 'On it...',
   error: 'I could not understand that..  try to rephrase and send it again.',
   help:
-    `💬💬💬${newLine(1)}Need a quick helper? Need a fast answer to a question?` +
-    `${newLine(2)}Start conversation with GPT.  Send informative prompts and get answers.` +
-    `${newLine(
-      2
-    )}💡 _Tip: start with the word gpt and try to make your prompts informative._${newLine(
-      2
-    )}${bold('Examples:')}${newLine(1)}_gpt what is the weather in Paris today?_${newLine(
-      1
-    )}_gpt please summarize Moby-Dick for my upcoming book report_${newLine(2)}${bold(
-      'Related commands:'
-    )}${newLine(1)}␥  Send ${bold(
-      'gpt-reset'
-    )} to reset your gpt history to break context and start fresh.${newLine(2)}_Send ${bold(
-      'HELP'
-    )} to return to main menu._`,
+    `🤖🤖🤖*Chat with GPT*` +
+    `${newLine(1)}Have insightful conversations with Fredo, powered by ChatGPT.` +
+    `${newLine(2)} *Keyword*`+
+    `${newLine(1)}gpt`+
+    `${newLine(2)}*Examples*:`+
+    `${newLine(1)}gpt help me plan a family trip`+
+    `${newLine(1)}gpt Give me inspiring quotes by Churchill`+
+    `${newLine(2)}*Related command*:`+
+    `${newLine(1)}Send “close” to end the session`,
 
   reset: `Your'e GPT session has been initialized. You can start a new conversation.`,
   failedResponseByOpenAi:
     'Seems like GPT service is not responding to us.\nPlease try again in few minutes.',
   failedResponseByOther:
     'This is awkward, something went wrong.\nWe are looking into it now.\n\n💡You can start a new session by sending *gpt* followed by your prompt.',
-  startSession: 'Starting ChatGPT session... \nTo end it send *End chat* at any point.',
+  startSession: 'Starting ChatGPT session... \nTo close it, send/tap ‘end chat’.',
   closeSession:
-    'GPT session has ended. If you want to start a new session just send *gpt* followed by your prompt.',
+    'The GPT session is now closed.',
   youCanSendCancel:
     'Note: To end ChatGPT session tap *End chat*.\nOtherwise, continue the conversation.',
   TapEndChatToClose: 'Tap *End chat* to end ChatGPT session.',
